55c43ac6a00bb93cbb0e5cfd81c9030185d8456d
Jeremiah Ogbomo <jeremiahogbomo@gmail.com>
2023-04-08 09:31:33 +0200

    Scaffold budget plans page

--- lib/presentation.dart
+++ lib/presentation.dart
@@ -1,5 +1,6 @@
 export 'presentation/app.dart';
 export 'presentation/screens/active_budget/active_budget_page.dart';
+export 'presentation/screens/budget_plans/budget_plans_page.dart';
 export 'presentation/state.dart';
 export 'presentation/theme.dart';
 export 'presentation/utils.dart';
---
+++ lib/presentation/screens/budget_plans/budget_plans_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class BudgetPlansPage extends StatefulWidget {
+  const BudgetPlansPage({super.key});
+
+  @override
+  State<BudgetPlansPage> createState() => _BudgetPlansPageState();
+}
+
+class _BudgetPlansPageState extends State<BudgetPlansPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Placeholder(),
+    );
+  }
+}
