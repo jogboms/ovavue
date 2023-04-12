bee529a4662e3ae21d3150b4495d9740bf5597d8
Jeremiah Ogbomo <jeremiahogbomo@gmail.com>
2023-04-12 08:20:56 +0200

    Scaffold grouped budget plans page

--- lib/presentation.dart
+++ lib/presentation.dart
@@ -7,6 +7,7 @@ export 'presentation/screens/budget_categories/budget_category_detail_page.dart'
 export 'presentation/screens/budget_categories/providers/selected_budget_category_provider.dart';
 export 'presentation/screens/budget_plans/budget_plan_detail_page.dart';
 export 'presentation/screens/budget_plans/budget_plans_page.dart';
+export 'presentation/screens/budget_plans/grouped_budget_plans_page.dart';
 export 'presentation/screens/budget_plans/providers/selected_budget_plan_provider.dart';
 export 'presentation/state.dart';
 export 'presentation/theme.dart';
